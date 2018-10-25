f46bfe16d0ed7814b47cb16e90f6bd77eb9a7400
Angelica Schell <Angelica@Angelicas-MacBook-Air.local>
2018-10-25 11:58:34 -0400

    changes made to syn dict, it is now working

--- Lousy_Plasma.py
+++ Lousy_Plasma.py
@@ -1,14 +1,39 @@
-new_list = []
-def lousy_plasma():
-    syn_dict = {"happy":"GLAD", "sad":"BLEAK"}
-    quest = input("What would you like to translate?") .strip() .lower()
-    quest = quest.split()
-    print(syn_dict[1])
+import string
+import random
 
+def remove_punctuation(var):
+    no_punct = ""
+    punctuation =  '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    for item in var: 
+        if item not in punctuation:
+            no_punct += item + " " 
+    return no_punct
+
+
+
+f = open('thesaurus.txt', 'r')
+syn_dict = {"happy" :["GLAD", "BLISSFUL", "ECSTATIC"], "sad":["BLEAK", "BLUE", "DEPRESSED"]}
+
+syn = {}
+for line in f:
+    split_words = line.split(',')
+    first_val = split_words.pop(0)
+    syn.update({first_val:split_words})
+print(syn)
+
+
+
+def lousy_plasma():
+    new_string = ""
+    quest = input("What would you like to translate?").strip()
+    quest = quest.split()
+    quest = remove_punctuation(quest).split()
     for item in quest:
-        if item in syn_dict.values() == True:
-              new_list.append(syn_dict[item])
-        elif item in syn_dict.values() == False:
-              new_list.append(item)
-    print(new_list) 
+        if item in syn.keys():
+            new_string += random.choice(syn[item]) + " "
+        else:
+            new_string += item + " "
+
+    print(new_string) 
 lousy_plasma()
+
