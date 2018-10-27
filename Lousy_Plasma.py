e649da4d7369249e5082a2a42fca1427c3766876
Angelica Schell <Angelica@Angelicas-MacBook-Air.local>
2018-10-27 11:53:10 -0400

    Everything is working except the speaking part

--- Lousy_Plasma.py
+++ Lousy_Plasma.py
@@ -1,39 +1,51 @@
 import string
 import random
+import os
+
 
 def remove_punctuation(var):
     no_punct = ""
     punctuation =  '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     for item in var: 
         if item not in punctuation:
-            no_punct += item + " " 
+            no_punct += item 
     return no_punct
 
 
-
+bad_blood_lyrics = open('bad_blood_lyrics.txt', 'r').read()
 f = open('thesaurus.txt', 'r')
-syn_dict = {"happy" :["GLAD", "BLISSFUL", "ECSTATIC"], "sad":["BLEAK", "BLUE", "DEPRESSED"]}
 
 syn = {}
 for line in f:
+    line = line.strip('\n')
     split_words = line.split(',')
     first_val = split_words.pop(0)
     syn.update({first_val:split_words})
-print(syn)
-
 
 
 def lousy_plasma():
-    new_string = ""
-    quest = input("What would you like to translate?").strip()
-    quest = quest.split()
-    quest = remove_punctuation(quest).split()
-    for item in quest:
+    user_input = input("What would you like to translate?").strip()
+    user_input_without_punct = remove_punctuation(user_input).split()
+    result = ""
+    for item in user_input_without_punct:
         if item in syn.keys():
-            new_string += random.choice(syn[item]) + " "
+            result += random.choice(syn[item]) + " "
         else:
-            new_string += item + " "
+            result += item + " "
+    return result 
 
-    print(new_string) 
-lousy_plasma()
+
+def word_changer(words):
+    words = words.strip()
+    words_without_punct = remove_punctuation(words).split()
+    result = ""
+    for item in words_without_punct:
+        if item in syn.keys():
+            result += random.choice(syn[item]) + " "
+        else:
+            result += item + " "
+    os.system("say" + result)
+
+
+print(word_changer(bad_blood_lyrics))
 
