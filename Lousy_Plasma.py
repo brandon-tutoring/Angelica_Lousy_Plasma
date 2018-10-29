0b1986d8129681f4be222ae5433512cbea10bdfc
Angelica Schell <Angelica@Angelicas-MacBook-Air.local>
2018-10-28 21:38:17 -0400

    It all works now

--- Lousy_Plasma.py
+++ Lousy_Plasma.py
@@ -1,6 +1,5 @@
 import string
 import random
-import os
 
 
 def remove_punctuation(var):
@@ -20,10 +19,12 @@ for line in f:
     line = line.strip('\n')
     split_words = line.split(',')
     first_val = split_words.pop(0)
+    for i in range(0, len(split_words)):
+        split_words[i] = split_words[i].upper()
     syn.update({first_val:split_words})
 
-
-def lousy_plasma():
+# No longer in use.
+'''def lousy_plasma():
     user_input = input("What would you like to translate?").strip()
     user_input_without_punct = remove_punctuation(user_input).split()
     result = ""
@@ -32,20 +33,26 @@ def lousy_plasma():
             result += random.choice(syn[item]) + " "
         else:
             result += item + " "
-    return result 
+    return result '''
 
 
 def word_changer(words):
     words = words.strip()
     words_without_punct = remove_punctuation(words).split()
     result = ""
+    c = 0
+        
     for item in words_without_punct:
         if item in syn.keys():
-            result += random.choice(syn[item]) + " "
+            if c % 2 == 0:
+                result += random.choice(syn[item])
+            else: 
+                result += item
+            c += 1
         else:
-            result += item + " "
-    os.system("say" + result)
-
+                result += item 
+        result += " " 
+    return result
 
 print(word_changer(bad_blood_lyrics))
 
